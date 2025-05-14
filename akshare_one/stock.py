7e57f275a4e8f7f091571fac9dd0a5bfefcbeb72
Darren Zeng <zwldarren@gmail.com>
2025-05-14 02:01:52 +0000

    Change default realtime data source from eastmoney to xueqiu

--- akshare_one/stock.py
+++ akshare_one/stock.py
@@ -61,7 +61,7 @@ def get_hist_data(
 
 
 def get_realtime_data(
-    source: str = "eastmoney", symbol: Optional[str] = None
+    source: str = "xueqiu", symbol: Optional[str] = None
 ) -> "pd.DataFrame":
     """Get real-time market quotes
 
